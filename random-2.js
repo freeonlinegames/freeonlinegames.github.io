cc1519423c93eab18a1605c8f89805f2638d53a2
freeonlinegames <104126475+freeonlinegames@users.noreply.github.com>
2023-02-16 04:10:30 +0700

    Update random-2.js

--- random-2.js
+++ random-2.js
@@ -1,86 +1,91 @@
 //** Text ad script by Petalia.org **//
 
 //specify total # of random links
-var totallinks=33
+var totallinks=25
 
 var urls=new Array(totallinks)
 var images=new Array(totallinks)
 var titles=new Array(totallinks)
 
-urls[4]="https://coolgames24h.blogspot.com/2022/12/zoom-be.html"
-images[4]="https://tpc.googlesyndication.com/simgad/7326914814368110332"
+urls[0]="https://coolgames24h.blogspot.com/2022/12/zoom-be.html"
+images[0]="https://tpc.googlesyndication.com/simgad/7326914814368110332"
 
-urls[5]="https://coolgames24h.blogspot.com/2023/01/sling-kong.html"
-images[5]="https://tpc.googlesyndication.com/simgad/12999649372533822255"
+urls[1]="https://coolgames24h.blogspot.com/2023/01/sling-kong.html"
+images[1]="https://tpc.googlesyndication.com/simgad/12999649372533822255"
 
-urls[8]="https://coolgames24h.blogspot.com/2023/01/brain-test-tricky-puzzles-halloween.html"
-images[8]="https://tpc.googlesyndication.com/simgad/12070360924591750553"
+urls[2]="https://coolgames24h.blogspot.com/2023/01/brain-test-tricky-puzzles-halloween.html"
+images[2]="https://tpc.googlesyndication.com/simgad/12070360924591750553"
 
-urls[11]="https://coolgames24h.blogspot.com/2023/01/bouncy-basketball.html"
-images[11]="https://tpc.googlesyndication.com/simgad/7410094582388000341"
+urls[3]="https://coolgames24h.blogspot.com/2023/01/bouncy-basketball.html"
+images[3]="https://tpc.googlesyndication.com/simgad/7410094582388000341"
 
-urls[30]="https://coolgames24h.blogspot.com/2022/12/gladihoppers.html"
-images[30]="https://tpc.googlesyndication.com/simgad/6510411956387609396"
+urls[4]="https://coolgames24h.blogspot.com/2022/12/gladihoppers.html"
+images[4]="https://tpc.googlesyndication.com/simgad/6510411956387609396"
 
-urls[34]="https://coolgames24h.blogspot.com/2023/01/backflipper.html"
-images[34]="https://tpc.googlesyndication.com/simgad/16647224633297190282"
+urls[5]="https://coolgames24h.blogspot.com/2023/01/backflipper.html"
+images[5]="https://tpc.googlesyndication.com/simgad/16647224633297190282"
 
-urls[38]="https://coolgames24h.blogspot.com/2023/01/apple-knight.html"
-images[38]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/40e8e10dd43e3be9cdbf4c32e1a225bc.png"
+urls[6]="https://coolgames24h.blogspot.com/2023/01/apple-knight.html"
+images[6]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/40e8e10dd43e3be9cdbf4c32e1a225bc.png"
 
-urls[42]="https://bit.ly/coolgames24h"
-images[42]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/5a8a57fd387c273b8ac9f6ca3e8f805e.jfif"
+urls[7]="https://bit.ly/coolgames24h"
+images[7]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/5a8a57fd387c273b8ac9f6ca3e8f805e.jfif"
 
-urls[44]="https://coolgames24h.blogspot.com/2023/01/blumgi-ball.html"
-images[44]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/806d700a19445959723fda1a6dc8dbc7.png"
+urls[8]="https://coolgames24h.blogspot.com/2023/01/blumgi-ball.html"
+images[8]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/806d700a19445959723fda1a6dc8dbc7.png"
 
-urls[45]="https://coolgames24h.blogspot.com/2022/12/monkey-mart.html"
-images[45]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/8ea2e5c9622f4a1c381cc637eaeb0257.jfif"
+urls[9]="https://coolgames24h.blogspot.com/2022/12/monkey-mart.html"
+images[9]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/8ea2e5c9622f4a1c381cc637eaeb0257.jfif"
 
-urls[58]="https://coolgames24h.blogspot.com/2023/01/stick-merge-halloween.html"
-images[58]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/dc961a44f1217aaf14db49e4e48271df.jfif"
+urls[10]="https://coolgames24h.blogspot.com/2023/01/stick-merge-halloween.html"
+images[10]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/dc961a44f1217aaf14db49e4e48271df.jfif"
 
-urls[60]="https://coolgames24h.blogspot.com/2022/12/truck-loader.html"
-images[60]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/ad02d80224b2cb6483a6274b534d713e.jfif"
+urls[11]="https://coolgames24h.blogspot.com/2022/12/truck-loader.html"
+images[11]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/ad02d80224b2cb6483a6274b534d713e.jfif"
 
-urls[63]="https://coolgames24h.blogspot.com/2023/01/who-is.html"
-images[63]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/47bc496c3b9a8b0ba17ecb97ff2d9178.jfif"
+urls[12]="https://coolgames24h.blogspot.com/2023/01/who-is.html"
+images[12]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/47bc496c3b9a8b0ba17ecb97ff2d9178.jfif"
 
-urls[68]="https://coolgames24h.blogspot.com/2023/01/like-king.html"
-images[68]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/e379e31a1b962ba8e39b16500117ec92.jfif"
+urls[13]="https://coolgames24h.blogspot.com/2023/01/like-king.html"
+images[13]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/e379e31a1b962ba8e39b16500117ec92.jfif"
 
-urls[75]="https://coolgames24h.blogspot.com/2023/01/bacon-may-die.html"
-images[75]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/46736308ca69fd787d53e85f39004ce0.png"
+urls[14]="https://coolgames24h.blogspot.com/2023/01/bacon-may-die.html"
+images[14]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/46736308ca69fd787d53e85f39004ce0.png"
 
-urls[82]="https://coolgames24h.blogspot.com/2022/12/zoom-be-2.html"
-images[82]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/dc8bfa481f328349c47c6aefed9146ec.jfif"
+urls[15]="https://coolgames24h.blogspot.com/2022/12/zoom-be-2.html"
+images[15]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/dc8bfa481f328349c47c6aefed9146ec.jfif"
 
-urls[84]="https://coolgames24h.blogspot.com/2023/01/beecoins-inc.html"
-images[84]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/a288eab44a75f30ebbd2fab2d8f5f1ba.png"
+urls[16]="https://coolgames24h.blogspot.com/2023/01/beecoins-inc.html"
+images[16]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/a288eab44a75f30ebbd2fab2d8f5f1ba.png"
 
-urls[85]="https://coolgames24h.blogspot.com/2023/01/stickman-climb-2.html"
-images[85]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/295250063c957da4e5d0e84fd38c6948.jfif"
+urls[17]="https://coolgames24h.blogspot.com/2023/01/stickman-climb-2.html"
+images[17]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/295250063c957da4e5d0e84fd38c6948.jfif"
 
-urls[88]="https://coolgames24h.blogspot.com/2023/01/foggy-fox.html"
-images[88]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/7b44fab39703e195c7a5ba66b64a5b05.jfif"
+urls[18]="https://coolgames24h.blogspot.com/2023/01/foggy-fox.html"
+images[18]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/7b44fab39703e195c7a5ba66b64a5b05.jfif"
 
-urls[91]="https://coolgames24h.blogspot.com/2022/12/puffy-cat.html"
-images[91]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/4bf601febc38ace11514995332ceabb1.png"
+urls[19]="https://coolgames24h.blogspot.com/2022/12/puffy-cat.html"
+images[19]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/4bf601febc38ace11514995332ceabb1.png"
 
-urls[95]="https://coolgames24h.blogspot.com/2022/12/duo-survival-3.html"
-images[95]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/223bb0d4abeb5ca1e14b9d9d5444180f.jfif"
+urls[20]="https://coolgames24h.blogspot.com/2022/12/duo-survival-3.html"
+images[20]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/223bb0d4abeb5ca1e14b9d9d5444180f.jfif"
 
-urls[98]="https://coolgames24h.blogspot.com/2023/02/sugar-sugar.html"
-images[98]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/867eb7da279dc1ffde18879d8964bd1b.png"
+urls[21]="https://coolgames24h.blogspot.com/2023/02/sugar-sugar.html"
+images[21]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/867eb7da279dc1ffde18879d8964bd1b.png"
 
-urls[107]="https://coolgames24h.blogspot.com/2023/01/blumgi-castle.html"
-images[107]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/79a3d286fbd540f1f746bbafeea12603.png"
+urls[22]="https://coolgames24h.blogspot.com/2023/01/blumgi-castle.html"
+images[22]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/79a3d286fbd540f1f746bbafeea12603.png"
 
-urls[109]="https://coolgames24h.blogspot.com/2022/12/duo-vikings.html"
-images[109]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/3ede4db12b0744f49c1293957564f7f6.jfif"
+urls[23]="https://coolgames24h.blogspot.com/2022/12/duo-vikings.html"
+images[23]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/3ede4db12b0744f49c1293957564f7f6.jfif"
 
-urls[112]="https://coolgames24h.blogspot.com/2022/06/subway-surfers-world-tour-singapore.html"
-images[112]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/c24b778b7daba00c857c17fd76acc5c5.jfif"
+urls[24]="https://coolgames24h.blogspot.com/2022/06/subway-surfers-world-tour-singapore.html"
+images[24]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/c24b778b7daba00c857c17fd76acc5c5.jfif"
 
-urls[115]="https://coolgames24h.blogspot.com/2023/01/kiwi-clicker.html"
-images[0]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/8ee29cd1534eabc779509e741e135364.png"
+urls[25]="https://coolgames24h.blogspot.com/2023/01/kiwi-clicker.html"
+images[25]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/8ee29cd1534eabc779509e741e135364.png"
+
+var randomgame=Math.floor(Math.random()*totallinks)
+var randomgame2=Math.floor(Math.random()*totallinks)
+var randomgame3=Math.floor(Math.random()*totallinks)
+var randomgame4=Math.floor(Math.random()*totallinks)
