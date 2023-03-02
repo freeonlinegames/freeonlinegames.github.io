27d94830ba9fd0eecdd00812314c59b683b0ed3e
freeonlinegames <104126475+freeonlinegames@users.noreply.github.com>
2023-03-02 11:17:54 +0700

    Update random-2.js

--- random-2.js
+++ random-2.js
@@ -7,88 +7,88 @@ var urls=new Array(totallinks)
 var images=new Array(totallinks)
 var titles=new Array(totallinks)
 
-urls[0]="https://unblockedgameszone.blogspot.com/2022/12/zoom-be.html"
+urls[0]="https://adclick.g.doubleclick.net/pcs/click?&adurl=https://unblockedgameszone.blogspot.com/2022/12/zoom-be.html"
 images[0]="https://tpc.googlesyndication.com/simgad/7326914814368110332"
 
-urls[1]="https://unblockedgameszone.blogspot.com/2023/01/sling-kong.html"
+urls[1]="https://adclick.g.doubleclick.net/pcs/click?&adurl=https://unblockedgameszone.blogspot.com/2023/01/sling-kong.html"
 images[1]="https://tpc.googlesyndication.com/simgad/12999649372533822255"
 
-urls[2]="https://unblockedgameszone.blogspot.com/2023/01/brain-test-tricky-puzzles-halloween.html"
+urls[2]="https://adclick.g.doubleclick.net/pcs/click?&adurl=https://unblockedgameszone.blogspot.com/2023/01/brain-test-tricky-puzzles-halloween.html"
 images[2]="https://tpc.googlesyndication.com/simgad/12070360924591750553"
 
-urls[3]="https://unblockedgameszone.blogspot.com/2023/01/bouncy-basketball.html"
+urls[3]="https://adclick.g.doubleclick.net/pcs/click?&adurl=https://unblockedgameszone.blogspot.com/2023/01/bouncy-basketball.html"
 images[3]="https://tpc.googlesyndication.com/simgad/7410094582388000341"
 
-urls[4]="https://unblockedgameszone.blogspot.com/2022/12/gladihoppers.html"
+urls[4]="https://adclick.g.doubleclick.net/pcs/click?&adurl=https://unblockedgameszone.blogspot.com/2022/12/gladihoppers.html"
 images[4]="https://tpc.googlesyndication.com/simgad/6510411956387609396"
 
-urls[5]="https://unblockedgameszone.blogspot.com/2023/01/backflipper.html"
+urls[5]="https://adclick.g.doubleclick.net/pcs/click?&adurl=https://unblockedgameszone.blogspot.com/2023/01/backflipper.html"
 images[5]="https://tpc.googlesyndication.com/simgad/16647224633297190282"
 
-urls[6]="https://unblockedgameszone.blogspot.com/2023/01/apple-knight.html"
+urls[6]="https://adclick.g.doubleclick.net/pcs/click?&adurl=https://unblockedgameszone.blogspot.com/2023/01/apple-knight.html"
 images[6]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/40e8e10dd43e3be9cdbf4c32e1a225bc.png"
 
-urls[7]="https://unblockedgameszone.blogspot.com/2022/12/party-toons.html"
+urls[7]="https://adclick.g.doubleclick.net/pcs/click?&adurl=https://unblockedgameszone.blogspot.com/2022/12/party-toons.html"
 images[7]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/5a8a57fd387c273b8ac9f6ca3e8f805e.jfif"
 
-urls[8]="https://unblockedgameszone.blogspot.com/2023/01/blumgi-ball.html"
+urls[8]="https://adclick.g.doubleclick.net/pcs/click?&adurl=https://unblockedgameszone.blogspot.com/2023/01/blumgi-ball.html"
 images[8]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/806d700a19445959723fda1a6dc8dbc7.png"
 
-urls[9]="https://unblockedgameszone.blogspot.com/2022/12/monkey-mart.html"
+urls[9]="https://adclick.g.doubleclick.net/pcs/click?&adurl=https://unblockedgameszone.blogspot.com/2022/12/monkey-mart.html"
 images[9]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/8ea2e5c9622f4a1c381cc637eaeb0257.jfif"
 
-urls[10]="https://unblockedgameszone.blogspot.com/2023/01/stick-merge-halloween.html"
+urls[10]="https://adclick.g.doubleclick.net/pcs/click?&adurl=https://unblockedgameszone.blogspot.com/2023/01/stick-merge-halloween.html"
 images[10]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/dc961a44f1217aaf14db49e4e48271df.jfif"
 
-urls[11]="https://unblockedgameszone.blogspot.com/2022/12/truck-loader.html"
+urls[11]="https://adclick.g.doubleclick.net/pcs/click?&adurl=https://unblockedgameszone.blogspot.com/2022/12/truck-loader.html"
 images[11]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/ad02d80224b2cb6483a6274b534d713e.jfif"
 
-urls[12]="https://unblockedgameszone.blogspot.com/2023/01/who-is.html"
+urls[12]="https://adclick.g.doubleclick.net/pcs/click?&adurl=https://unblockedgameszone.blogspot.com/2023/01/who-is.html"
 images[12]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/47bc496c3b9a8b0ba17ecb97ff2d9178.jfif"
 
-urls[13]="https://unblockedgameszone.blogspot.com/2023/01/like-king.html"
+urls[13]="https://adclick.g.doubleclick.net/pcs/click?&adurl=https://unblockedgameszone.blogspot.com/2023/01/like-king.html"
 images[13]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/e379e31a1b962ba8e39b16500117ec92.jfif"
 
-urls[14]="https://unblockedgameszone.blogspot.com/2023/01/bacon-may-die.html"
+urls[14]="https://adclick.g.doubleclick.net/pcs/click?&adurl=https://unblockedgameszone.blogspot.com/2023/01/bacon-may-die.html"
 images[14]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/46736308ca69fd787d53e85f39004ce0.png"
 
-urls[15]="https://unblockedgameszone.blogspot.com/2022/12/zoom-be-2.html"
+urls[15]="https://adclick.g.doubleclick.net/pcs/click?&adurl=https://unblockedgameszone.blogspot.com/2022/12/zoom-be-2.html"
 images[15]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/dc8bfa481f328349c47c6aefed9146ec.jfif"
 
-urls[16]="https://unblockedgameszone.blogspot.com/2023/01/beecoins-inc.html"
+urls[16]="https://adclick.g.doubleclick.net/pcs/click?&adurl=https://unblockedgameszone.blogspot.com/2023/01/beecoins-inc.html"
 images[16]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/a288eab44a75f30ebbd2fab2d8f5f1ba.png"
 
-urls[17]="https://unblockedgameszone.blogspot.com/2023/01/stickman-climb-2.html"
+urls[17]="https://adclick.g.doubleclick.net/pcs/click?&adurl=https://unblockedgameszone.blogspot.com/2023/01/stickman-climb-2.html"
 images[17]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/295250063c957da4e5d0e84fd38c6948.jfif"
 
-urls[18]="https://unblockedgameszone.blogspot.com/2023/01/foggy-fox.html"
+urls[18]="https://adclick.g.doubleclick.net/pcs/click?&adurl=https://unblockedgameszone.blogspot.com/2023/01/foggy-fox.html"
 images[18]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/7b44fab39703e195c7a5ba66b64a5b05.jfif"
 
-urls[19]="https://unblockedgameszone.blogspot.com/2022/12/puffy-cat.html"
+urls[19]="https://adclick.g.doubleclick.net/pcs/click?&adurl=https://unblockedgameszone.blogspot.com/2022/12/puffy-cat.html"
 images[19]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/4bf601febc38ace11514995332ceabb1.png"
 
-urls[20]="https://unblockedgameszone.blogspot.com/2022/12/duo-survival-3.html"
+urls[20]="https://adclick.g.doubleclick.net/pcs/click?&adurl=https://unblockedgameszone.blogspot.com/2022/12/duo-survival-3.html"
 images[20]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/223bb0d4abeb5ca1e14b9d9d5444180f.jfif"
 
-urls[21]="https://unblockedgameszone.blogspot.com/2023/01/sugar-sugar.html"
+urls[21]="https://adclick.g.doubleclick.net/pcs/click?&adurl=https://unblockedgameszone.blogspot.com/2023/01/sugar-sugar.html"
 images[21]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/867eb7da279dc1ffde18879d8964bd1b.png"
 
-urls[22]="https://unblockedgameszone.blogspot.com/2023/01/blumgi-castle.html"
+urls[22]="https://adclick.g.doubleclick.net/pcs/click?&adurl=https://unblockedgameszone.blogspot.com/2023/01/blumgi-castle.html"
 images[22]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/79a3d286fbd540f1f746bbafeea12603.png"
 
-urls[23]="https://unblockedgameszone.blogspot.com/2022/12/duo-vikings.html"
+urls[23]="https://adclick.g.doubleclick.net/pcs/click?&adurl=https://unblockedgameszone.blogspot.com/2022/12/duo-vikings.html"
 images[23]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/3ede4db12b0744f49c1293957564f7f6.jfif"
 
-urls[24]="https://unblockedgameszone.blogspot.com/2022/06/subway-surfers-singapore.html"
+urls[24]="https://adclick.g.doubleclick.net/pcs/click?&adurl=https://unblockedgameszone.blogspot.com/2022/06/subway-surfers-singapore.html"
 images[24]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/c24b778b7daba00c857c17fd76acc5c5.jfif"
 
-urls[25]="https://unblockedgameszone.blogspot.com/2023/01/kiwi-clicker.html"
+urls[25]="https://adclick.g.doubleclick.net/pcs/click?&adurl=https://unblockedgameszone.blogspot.com/2023/01/kiwi-clicker.html"
 images[25]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/8ee29cd1534eabc779509e741e135364.png"
 
-urls[26]="https://unblockedgameszone.blogspot.com/2023/01/crazy-cars.html"
+urls[26]="https://adclick.g.doubleclick.net/pcs/click?&adurl=https://unblockedgameszone.blogspot.com/2023/01/crazy-cars.html"
 images[26]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/8961782aa2bc9f49e2dac04d42a587c0.jfif"
 
-urls[27]="https://unblockedgameszone.blogspot.com/2023/01/blumgi-rocket.html"
+urls[27]="https://adclick.g.doubleclick.net/pcs/click?&adurl=https://unblockedgameszone.blogspot.com/2023/01/blumgi-rocket.html"
 images[27]="https://ads.poki.com/cdn-cgi/image/quality=78,f=auto,w=160,h=600,fit=cover/creatives/08389a3ba2fd6929e918056ea5a5b155.jfif"
 
 var randomgame=Math.floor(Math.random()*totallinks)
